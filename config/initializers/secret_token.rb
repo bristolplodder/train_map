6de4febbee67ff92056db277a7c8e2876100d9ad
bristolplodder <matthew.cockburn@blueyonder.co.uk>
2012-12-14 11:20:56 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrainMap::Application.config.secret_token = '3049be85d3f79556ee4d8093c563331ea582afeb6c9d572e0bfa61e0ace71e0f0aa95b7778e12708c97ccf9dbf5b3dea2cd18622c9ca4e19dc944f03d83d4268'
